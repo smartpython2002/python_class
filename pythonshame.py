32fa0eed34f9add05869c3624d82c6349b63eff4
smartpython2002 <abrigailpratcher961@gmail.com>
2019-07-20 12:21:33 -0700

    add loop

--- pythonshame.py
+++ pythonshame.py
@@ -16,7 +16,10 @@ def calculate(first_number, second_number, operator):
         result = OUT_OF_BOUNDS
     return result
 
+# print (calculate(1,2, '+'))
 
+# set a boolean to true to continue asking for input 
+more_input = True
 
 first_number = input("What is the first number? ")
 first_number =int(first_number) 
@@ -33,3 +36,12 @@ if result == OUT_OF_BOUNDS:
     print("you need valid operator(+, -, *, /)?")
 else:
     print (result)
+
+if input("Another calculation (y/n)? ") == "y":
+    more_input = True
+else:
+    more_input = False
+
+#more_input = True if input("Another calculation (y/n)?")== 'y' else False
+
+print ("Bye now")
